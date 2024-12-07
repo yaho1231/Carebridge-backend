4b86d37bb96d395fbc3f527200ba6041e1ecb47c
gepetton <sehyeon73@gmail.com>
2024-12-07 15:10:23 +0900

    초기 환경 세팅

---
+++ src/test/java/com/example/carebridge/CarebridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carebridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarebridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
